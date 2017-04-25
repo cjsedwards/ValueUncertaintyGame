2b1590dfc12c6533ad93d99edc07e6e5f62f561e
Chris Edwards <chris.edwards@yyc-cedwards-mp.hitronhub.home>
2017-04-24 22:55:54 -0600

    Trying again with compile task with outputs

--- build.sh
+++ build.sh
@@ -4,3 +4,5 @@ set -e -x
 
 nuget restore ValueUncertaintyGame/ValueUncertaintyGame.sln
 xbuild ValueUncertaintyGame/ValueUncertaintyGame.sln
+cp ValueUncertaintyGame/ValueUncertaintyGameRunner/bin/Debug/*.* ValueUncertaintyGameBuildResults
+cp ValueUncertaintyGame/Dockerfile ValueUncertaintyGameBuildResults
