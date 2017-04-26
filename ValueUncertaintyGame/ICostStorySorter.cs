93d995291f5ba2372920a867f497757e95e8ec9a
Chris Edwards <chris.edwards@yyc-cedwards-mp.sumyyc01.local>
2017-04-25 19:08:36 -0600

    break the build

--- ValueUncertaintyGame/ICostStorySorter.cs
+++ ValueUncertaintyGame/ICostStorySorter.cs
@@ -2,7 +2,7 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 
 namespace ValueUncertaintyGame
-{
+
 	public interface ICostStorySorter
 	{
 		List<IStory> Sort( ConcurrentDictionary<IStory, ValueCostEstimate> estimates );
